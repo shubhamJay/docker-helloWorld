97f748c91ca5ae8c56645190e81071531cf78cf8
Shubham <shubhamj7997@gmail.com>
2019-02-12 20:21:33 +0530

    Add landing page to take todo title and description

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express =require("express");
 const port = 3000;
 
+const homePage = `<form method=POST action=/todo><input name="title" placeholder="title"/>
+<input name="description" placeholder="description"/><button type="submit">submit</button></form>`;
+
 let app = express();
-app.get("/",(req,res)=>res.send("Hello"));
+app.use(express.urlencoded({extended:false}))
+app.get("/",(req,res)=>res.send(homePage));
+app.post("/todo",(req,res)=>{res.redirect("/")})
+
 app.listen(port,()=>{console.log(`listening in ${port}`)});
